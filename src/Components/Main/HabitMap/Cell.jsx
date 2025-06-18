152a96e9cb587f23ae4b63d18735ca8a9e16e189
ItsGunesh <itsme.gunesh87@gmail.com>
2025-06-18 23:08:08 +0530

    Setting up contextAPI

--- src/Components/Main/HabitMap/Cell.jsx
+++ src/Components/Main/HabitMap/Cell.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
 
 const Cell = () => {
-  const [isComplete,setIsComplete]=useState(false)
   const habits = ["Wake Up", "Meditate", "Gym", "DSA", "Web Dev", "Diet", "Junk", "Social", "Skin Care", "Reading"]
+  const [completedHabits, setCompletedHabits] = useState(new Array(habits.length).fill(false))
+
+  const toggleHabit = (index) => {
+    const newCompletedHabits = [...completedHabits]
+    newCompletedHabits[index] = !newCompletedHabits[index]
+    setCompletedHabits(newCompletedHabits)
+  }
+
   return (
-    <><div className='flex flex-col items-center gap-1 py-0.5'>
-        {habits.map((item,index)=>{
-          return <div key={index} className={`w-3 h-3 bg-gray-400 rounded-sm ${isComplete? 'bg-green-500': 'bg-gray-400'}`} onClick={(e)=>e.target(setIsComplete(!isComplete))}></div>
-        })}      
+    <div className='flex flex-col items-center gap-1 py-0.5'>
+      {habits.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className={`w-3 h-3 rounded-sm cursor-pointer ${
+              completedHabits[index] ? 'bg-green-500' : 'bg-gray-400'
+            }`}
+            onClick={() => toggleHabit(index)}
+          ></div>
+        )
+      })}
     </div>
-      
-    </>
   )
 }
 
