4a54015f7048bbea8df57148100368c76295897a
ItsGunesh <itsme.gunesh87@gmail.com>
2025-07-03 22:05:53 +0530

    Routers and controllers Setup

--- server/app.js
+++ server/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+import userRouter from "./routes/user.router.js"
+import { userRegister } from "./controllers/user.controller.js"
+
+app.use("/users",userRouter)
+
 export {app}
