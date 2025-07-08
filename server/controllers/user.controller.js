dfe4b2290af518c67e75bba3c1155e616225cb4b
ItsGunesh <itsme.gunesh87@gmail.com>
2025-07-08 11:56:22 +0530

    registerUser controller updated

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,7 @@
+import { model } from "mongoose"
+import { ApiError } from "../utils/apiError.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {User} from "../models/user.model.js"
 
 const userRegister = asyncHandler((req,res)=>{
     res.status(200).
@@ -8,6 +11,46 @@ const userRegister = asyncHandler((req,res)=>{
 
     const {username,email,password,fullName} = req.body
     console.log(email)
+
+    if(username === ""){
+        throw new ApiError(400,"Username required");
+    }
+    if(email === ""){
+        throw new ApiError(400,"Email required");
+    }
+    if(password === ""){
+        throw new ApiError(400,"Password required");
+    }
+    if(fullName === ""){
+        throw new ApiError(400,"FullName required");
+    }
+
+    const existingUser = User.findOne({
+        $or: [{username},{email}]
+    })
+    
+    if(existingUser){
+        throw new ApiError(409,"Username or email already exists")
+    }
+
+    const user = User.create({
+        fullName,
+        email,
+        password,
+        username : username.toLowerCase()
+    })
+
+    const createdUser = User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
 })
 
 
