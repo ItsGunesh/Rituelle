4c8fc2e7c1d20ddba2c1109fbc6abc414af8b6e3
ItsGunesh <itsme.gunesh87@gmail.com>
2025-07-14 23:32:32 +0530

    Updated code with frontend routing check

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,23 @@ import {HabitContextProvider} from './Context/HabitContextProvider.jsx'
 import LoginIndex from './Components/Login/LoginIndex.jsx'
 import SignUpIndex from './Components/SignUp/SignUpIndex.jsx'
 import HabitsTodoIndex from './Components/HabitsInput/HabitsTodoIndex.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Components/Main/Dashboard.jsx'
 
 
 function App() {
   return (
     <>
       <HabitContextProvider>
-      {/* <div className='sticky top-0 z-999'>
-        <Navigator/>
-      </div> */}
-      {/* <div className='bg-gray-200 h-[100%] w-[100%] py-4 px-10 box-border flex gap-4'>
-        <HabitsIndex/>
-        <TaskIndex/>
-      </div>
-      <div>
-      </div> */}
-      {/* <LoginIndex/> */}
-      {/* <SignUpIndex/> */}
+      
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/login" element={<LoginIndex/>} />
+            <Route path="/signup" element={<SignUpIndex/>} />
+            <Route path="/user" element={<HabitsIndex/>} />
+          </Routes>
+        </BrowserRouter>
       <HabitsTodoIndex/>
       </HabitContextProvider>
     </>
---
+++ src/Components/Main/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navigator from '../Navigator/Navigator.jsx'
+import TaskIndex from '../Main/Tasks/TaskIndex.jsx'
+import HabitsIndex from '../Main/HabitMap/HabitsIndex.jsx'
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className='sticky top-0 z-999'>
+        <Navigator/>
+      </div>
+      <div className='bg-gray-200 h-[100%] w-[100%] py-4 px-10 box-border flex gap-4'>
+        <HabitsIndex/>
+        <TaskIndex/>
+      </div>
+      <div>
+      </div>
+
+    </>
+  )
+}
+
+export default Dashboard
