83b155b80eb1ef1a4c12470a73d716d67035d146
ItsGunesh <itsme.gunesh87@gmail.com>
2025-07-25 21:42:46 +0530

    Cors

--- Backend/server/app.js
+++ Backend/server/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin:"http://localhost:5173", // get it from .env variables
+    origin:"*", // get it from .env variables
     credentials:true
 }))
 
