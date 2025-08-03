f42dc9362ea8f32dc0a4a3fd69e73e4f2265dc61
ItsGunesh <itsme.gunesh87@gmail.com>
2025-08-03 11:28:25 +0530

    Final Updates V1

--- Frontend/src/Components/Loader.jsx
+++ Frontend/src/Components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => (
     <div className="flex justify-center items-center">
-      <svg className="animate-spin h-6 w-6 text-purple-600" viewBox="0 0 24 24">
+      <svg className="animate-spin h-6 w-6 text-gray-900" viewBox="0 0 24 24">
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none" />
         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
       </svg>
